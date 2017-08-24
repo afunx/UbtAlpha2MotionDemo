29f35d2a7ec009474f3c22b28b2f2b5391d0dc0b
白桦 <afunx.bai@ubtrobot.com>
2017-08-24 15:14:26 +0800

    add MotionRobotApi2 demo code

--- app/src/main/java/com/ubt/ubtalpha2motiondemo/MainActivity.java
+++ app/src/main/java/com/ubt/ubtalpha2motiondemo/MainActivity.java
@@ -6,6 +6,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.ubtechinc.cruzr.sdk.motion.MotionRobotApi2;
+import com.ubtechinc.cruzr.serverlibutil.aidl.ActionMotorInfo;
+import com.ubtechinc.cruzr.serverlibutil.interfaces.ActionMotorFrameListener2;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "MainActivity";
@@ -18,9 +25,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initViews();
+
+        motionApi2Initialize();
+    }
+
+    /**
+     * MotionRobotApi2初始化
+     */
+    private void motionApi2Initialize() {
+        Log.i(TAG, "motionApi2Initialize()");
+        MotionRobotApi2.getInstance().initialize(this);
+        MotionRobotApi2.getInstance().setServiceBindedCallback(new Runnable() {
+            @Override
+            public void run() {
+                Log.i(TAG, "MotionRobotApi2 is binded");
+            }
+        });
+        MotionRobotApi2.getInstance().setServiceUnbindedCallback(new Runnable() {
+            @Override
+            public void run() {
+                Log.i(TAG, "MotionRobotApi2 is unbinded");
+            }
+        });
+    }
+
+    private void initViews() {
         mBtnEnterReadbackMode = (Button) findViewById(R.id.btnEnterReadbackMode);
         mBtnEnterReadbackMode.setOnClickListener(this);
 
@@ -62,6 +96,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     private void doEnterReadbackMode() {
         Log.i(TAG, "doEnterReadbackMode()");
+        new Thread() {
+            @Override
+            public void run() {
+                Log.i(TAG, "doEnterReadbackMode() start");
+                boolean enterReadMode = MotionRobotApi2.getInstance().enterReadMode();
+                Log.i(TAG, "doEnterReadbackMode() end, enterReadMode: " + enterReadMode);
+            }
+        }.start();
     }
 
     /**
@@ -69,6 +111,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     private void doExitReadbackMode() {
         Log.i(TAG, "doExitReadbackMode()");
+        new Thread() {
+            @Override
+            public void run() {
+                Log.i(TAG, "doExitReadbackMode() start");
+                boolean exitReadMode = MotionRobotApi2.getInstance().exitReadMode();
+                Log.i(TAG, "doExitReadbackMode() end, exitReadMode: " + exitReadMode);
+            }
+        }.start();
     }
 
     /**
@@ -76,6 +126,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     private void doCheckReadbackMode() {
         Log.i(TAG, "doCheckReadbackMode()");
+        new Thread() {
+            @Override
+            public void run() {
+                Log.i(TAG, "doCheckReadbackMode() start");
+                // 1表示进入readMode，-1表示退出readMode，0表示不确定
+                int isReadMode = MotionRobotApi2.getInstance().isReadMode();
+                Log.i(TAG, "doCheckReadbackMode() end, isReadMode: " + isReadMode);
+            }
+        }.start();
     }
 
     /**
@@ -83,13 +142,86 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     private void doGetMotorDegrees() {
         Log.i(TAG, "doGetMotorDegrees()");
+        new Thread() {
+            @Override
+            public void run() {
+                Log.i(TAG, "doGetMotorDegrees() start");
+                int[] motorsId = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+                // 舵机角度或-1
+                int[] motorsDegree = new int[motorsId.length];
+                MotionRobotApi2.getInstance().getMotorsDegree(motorsId, motorsDegree);
+                Log.i(TAG, "doGetMotorDegrees() end");
+                for (int i=0;i<motorsId.length;i++) {
+                    Log.i(TAG, "motorId: " + motorsId[i] + ", motorDegree: " + motorsDegree[i]);
+                }
+            }
+        }.start();
     }
+
+    private int _setMotorDegreesCount = 0;
+
+    // 帧动作id，用于舵机运动取消
+    private int[] mFrameId = new int[1];
 
     /**
      * 设置舵机角度
      */
     private void doSetMotorDegrees() {
         Log.i(TAG, "doSetMotorDegrees()");
+        new Thread() {
+            @Override
+            public void run() {
+                Log.i(TAG, "doSetMotorDegrees() start");
+                List<ActionMotorInfo> motorInfos = new ArrayList<ActionMotorInfo>();
+
+                final int[] ids = new int[]{2, 5};
+                ++_setMotorDegreesCount;
+
+                for (int i = 0; i < ids.length; i++) {
+                    ActionMotorInfo actionMotorInfo = new ActionMotorInfo();
+                    // 舵机最小角度值，实际每个舵机会有不同
+                    int lowerLimitAngle = 0;
+                    // 舵机最大角度值，实际每个舵机会有不同
+                    int upperLimitAngle = 360;
+                    int angle = _setMotorDegreesCount % 2 == 0 ? lowerLimitAngle : upperLimitAngle;
+                    int delayMilli = 0;
+                    int runMilli = 5000;
+
+                    actionMotorInfo.setMotorId(ids[i]);
+                    actionMotorInfo.setAbsoluteDegree(angle);
+                    actionMotorInfo.setDelayMilli(delayMilli);
+                    actionMotorInfo.setRunMilli(runMilli);
+
+                    motorInfos.add(actionMotorInfo);
+                }
+
+
+
+                ActionMotorFrameListener2 listener2 = new ActionMotorFrameListener2() {
+                    @Override
+                    public void onStart() {
+                        Log.i(TAG, "doSetMotorDegrees() onStart()");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.i(TAG, "doSetMotorDegrees() onComplete()");
+                    }
+
+                    @Override
+                    public void onError(int i) {
+                        Log.i(TAG, "doSetMotorDegrees() onError() i: " + i);
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Log.i(TAG, "doSetMotorDegrees() onCancel()");
+                    }
+                };
+                mFrameId[0] = MotionRobotApi2.getInstance().controlMotors2(motorInfos, listener2);
+                Log.i(TAG, "doSetMotorDegrees() end, mFrameId[0]: " + mFrameId[0]);
+            }
+        }.start();
     }
 
     /**
@@ -97,5 +229,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     private void doCancelMotorDegrees() {
         Log.i(TAG, "doCancelMotorDegrees()");
+        new Thread() {
+            @Override
+            public void run() {
+                Log.i(TAG, "doCancelMotorDegrees() start");
+                final boolean isCancelled = MotionRobotApi2.getInstance().cancelActionMotorFrame2(mFrameId[0]);
+                Log.i(TAG, "doCancelMotorDegrees() end, isCancelled: " + isCancelled);
+            }
+        }.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        motionApi2Destroy();
+    }
+
+    private void motionApi2Destroy() {
+        Log.i(TAG, "motionApi2Destroy()");
+        MotionRobotApi2.getInstance().destroy();
     }
 }
